66b34d55dc4b3e39422f5dc9960a84bff1e95f96
Ian Chen <ianre657@gmail.com>
2018-04-20 14:27:58 +0800

    include unistd.h in header& fully utilize cpu

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
+num_thread=`nproc`
 
 cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j$num_thread
 
 cd ../../g2o
 
@@ -13,7 +14,8 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+#make -j8
+make -j$num_thread
 
 cd ../../../
 
@@ -28,4 +30,5 @@ echo "Configuring and building ORB_SLAM2 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+#make -j8
+make -j$num_thread
